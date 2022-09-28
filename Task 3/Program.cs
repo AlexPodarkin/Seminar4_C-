49408a0aad52c928cdf6d07fa7ca845fb3fb5aa6
Aleksandr <alexchembarov@gmail.com>
2022-09-28 11:22:09 +0400

    Task 3

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -27,3 +27,35 @@ int[] array = new int[7];
 
 FillArray(array);
 PrintArray(array);
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArr(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+PrintArr(arr);
+SelectionSort(arr);
+
+PrintArr(arr);
